fa70a6878a359c499ddf5d1c80eb4c683c280c41
Oladayo-Ahmod <57647734+Oladayo-Ahmod@users.noreply.github.com>
2020-08-02 18:45:30 -0700

    Add files via upload

---
+++ unitech/form.php
@@ -0,0 +1,13 @@
+<?php 
+	#require_once('index.html');
+	if (isset($_POST['submit'])) {
+		$name = $_POST['name'];
+		$email = $_POST['email'];
+		$number = $_POST['number'];
+		$brand = $_POST['brand'];
+		$business = $_POST['business'];
+		$message = $_POST['message'];
+		$file = $_FILES['file']['name'];
+		
+	}
+ ?>
